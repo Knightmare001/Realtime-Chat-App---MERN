690c5369a35448dfb94800ceb55af2c61c9c21a3
Egi Prayogi <egi007p@gmail.com>
2026-03-14 21:14:18 +0700

    setup mongo database for user model

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import env from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+env.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server runing on port ${PORT}`);
+  connectDB();
+});
---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", (req, res) => {
+  res.send("signup route");
+});
+router.post("/logout", (req, res) => {
+  res.send("signup route");
+});
+
+export default router;
